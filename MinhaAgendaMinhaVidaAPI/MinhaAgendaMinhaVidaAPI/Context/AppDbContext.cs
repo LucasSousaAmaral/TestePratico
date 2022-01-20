ce7b982581379b12f765cf07a856bdb455e9ad71
Lucas Amaral <lucasdovah@gmail.com>
2022-01-19 22:57:02 -0300

    Adding data on model creating to populate database.

--- MinhaAgendaMinhaVidaAPI/MinhaAgendaMinhaVidaAPI/Context/AppDbContext.cs
+++ MinhaAgendaMinhaVidaAPI/MinhaAgendaMinhaVidaAPI/Context/AppDbContext.cs
@@ -5,12 +5,48 @@ namespace MinhaAgendaMinhaVidaAPI.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
 
         public DbSet<Agenda> Agendas { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    UserId = 1,
+                    Name = "Amaral",
+                    Login = "LucasAmaral",
+                    Password = "Amaral",
+                    Role = Role.Admin,
+                    Agendas = null
 
+
+                },
+                new User
+                {
+                    UserId = 2,
+                    Name = "Mateus",
+                    Login = "MateusAmaral",
+                    Password = "Mateus",
+                    Role = Role.Common,
+                    Agendas = null
+                }
+
+                );
+
+            modelBuilder.Entity<Agenda>().HasData(
+                new Agenda
+                {
+                    AgendaId = 1,
+                    Title = "Agenda Dezembro",
+                    Description = "Anotações sobre dezembro",
+                    Data = System.DateTime.Now,
+                }
+
+                );
+        }
     }
 }
